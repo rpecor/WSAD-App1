dca592d1f672a5fd9db41bed4399147069ce94af
Ryan Pecor <pecorrt@mail.uc.edu>
2016-01-19 22:36:57 -0500

    End of first assignment submission.

---
+++ WSAD-App1/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WSAD_App1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return this.RedirectToAction("Login");
+        }
+
+        /// <summary>
+        /// To Create a user account for my application
+        /// </summary>
+        /// <returns>ViewResult for the create</returns>
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Logging users into the web site
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password, IEnumerable<bool> rememberMe)
+        {
+                return Content("Hello " + username + ". Welcome to my website!"+ rememberMe);
+            
+            //Validate a username and password is passed (no empties)
+            // open database connection
+            //hash password
+            //query for user based on username and password hash
+            //if invalid, send error
+            //valid, redirect to user profile
+        }
+      
+    }
+}
