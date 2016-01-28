3e942b20ee1cfa041e41e775049b6de89f3138c9
Ryan Pecor <pecorrt@mail.uc.edu>
2016-01-27 19:44:18 -0500

    Correspondence form. Login - in progress.

--- WSAD-App1/Controllers/AccountController.cs
+++ WSAD-App1/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WSAD_App1.Models.ViewModels.Account;
 
 namespace WSAD_App1.Controllers
 {
@@ -34,16 +36,36 @@ namespace WSAD_App1.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Login(string username, string password, IEnumerable<bool> rememberMe)
+        public ActionResult Login(LoginUserViewModel loginUser)
         {
-                return Content("Hello " + username + ". Welcome to my website!"+ rememberMe);
-            
+
             //Validate a username and password is passed (no empties)
+            if (loginUser == null)
+            {
+                ModelState.AddModelError("", "Login is required");
+                return View();
+            }
+
+            if (string.IsNullOrWhiteSpace(loginUser.Username))
+            {
+                ModelState.AddModelError("", "Username is required");
+                return View();
+            }
+
+            if (string.IsNullOrWhiteSpace(loginUser.Password))
+            {
+                ModelState.AddModelError("", "Password is required");
+                return View();
+
+            }
             // open database connection
             //hash password
             //query for user based on username and password hash
             //if invalid, send error
             //valid, redirect to user profile
+            System.Web.Security.FormsAuthentication.SetAuthCookie(loginUser.Username, loginUser.RememberMe);
+
+            return Redirect(FormsAuthentication.GetRedirectUrl(loginUser.Username, loginUser.RememberMe));
         }
       
     }
---
+++ WSAD-App1/Controllers/CorrespondenceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WSAD_App1.Models.ViewModels.Correspondence;
+
+namespace WSAD_App1.Controllers
+{
+    public class CorrespondenceController : Controller
+    {
+        // GET: Correspondence
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ContactEmailViewModel contactMessage)
+        {
+            return null;
+        }
+    }
+}
